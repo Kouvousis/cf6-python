c136c1e124e863bf42f836ac93fac112c00ad320
Kouvousis <dexous1996@gmail.com>
2024-11-11 22:11:20 +0200

    initial commit

---
+++ chapter01/02_literals.py
@@ -0,0 +1,35 @@
+num1 = 10
+num2 = 0b1010
+num3 = 0x000A
+
+# strings
+str1 = 'CF1'
+str2 = "CF"
+str3 = 'Cpd\
+    ing'
+    
+# multiline strings
+str4 = '''Hello 
+Coding
+Factory'''
+
+str5 = """Hello
+again!"""
+
+str6 = "That's my name"
+
+str7 = 'Thats is "nested string"'
+
+my_bool = True
+
+print(type(num1))
+print(type(num2))
+print(type(num3))
+print(type(str1))
+print(type(str2))
+print(type(str3))
+print(type(str4))
+print(type(str5))
+print(type(str6))
+print(type(str7))
+print(type(my_bool))
---
+++ chapter01/out_of_the_box.py
@@ -0,0 +1,10 @@
+# 1
+name = "Alice"
+
+print(name)
+print = 10
+# print(10)
+
+del print
+
+print("print again", name)
