7df1204d4214df0ce43fa400acaeb978dd55546e
Kouvousis <dexous1996@gmail.com>
2024-12-03 20:37:55 +0200

    chapter 4

--- chapter04/17_reduce_demo.py
+++ chapter04/17_reduce_demo.py
@@ -5,7 +5,7 @@ my_ints = [1, 2 ,3, 4, 5]
 
 # return a result: 1 * 2 * 3 * 4 * 5
 result = reduce(lambda x, y: x * y, my_ints)
-print(result)
+print("Result:", result)
 
-result2 = reduce(lambda x, y: x * y, my_ints, 1)
-print(result2)
+result2 = reduce(lambda x, y: x * y, my_ints, 100)
+print("New result:", result2)
